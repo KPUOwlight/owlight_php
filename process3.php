e772341dcd3670d4a287ca3d00d7f39f911ea275
semonemo95 <39257629+semonemo95@users.noreply.github.com>
2021-06-19 16:45:08 +0900

    추가
    데이터베이스 변경으로 인한 코드 파일 추가

---
+++ process3.php
@@ -0,0 +1,34 @@
+<?php
+$con=mysqli_connect("localhost", "root", "root", "test");
+
+mysqli_set_charset($con,"utf8");
+
+
+if (mysqli_connect_errno($con)){
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$temp = $_GET['temp'];
+$humi = $_GET['humi'];
+$dust = $_GET['dust'];
+$battery = $_GET['bat'];
+
+/*echo "<br/>Temperature = $temp";
+echo ",";
+echo "Humidity = $humi";
+echo ",";
+echo "Dust = $dust";
+echo ","
+echo "Battery = $battery<br>"*/
+
+$query = "INSERT INTO test3 (temp, humi, dust, battery) VALUES ('$temp', '$humi', '$dust', '$battery')";
+$re = mysqli_query($con, $query);
+
+
+if($re) {
+  echo"</br>sucsess!!";
+} else{
+  echo 'fail';
+}
+  mysqli_close($con);
+?>
